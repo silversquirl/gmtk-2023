630f1d6ee1d5da5889aa893d72980029c4ac6144
Silver <silver@silv.re>
2023-07-09 14:13:11 +0100

    feat: implement player walk animations

--- Player.gd
+++ Player.gd
@@ -1,15 +1,39 @@
 extends AnimatedSprite2D
 
 @onready var map: TileMap = $".."
+var direction := Vector2i(0, 1)
+var lerp := 0.0
 @onready var map_pos: Vector2i = map.local_to_map(position):
 	set(pos):
+		if pos == map_pos: return
+		direction = pos - map_pos
+		lerp = 1
 		map_pos = pos
-		position = map.map_to_local(map_pos)
 
 func _ready():
 	%AITimer.timeout.connect(ai_step)
 
-func _process(_dt):
+func _process(dt):
+	var offset := -lerp * Vector2(direction * map.tile_set.tile_size)
+	position = map.map_to_local(map_pos) + offset
+
+	scale.x = 1
+	match direction:
+		Vector2i(0, 1):
+			play("walk_down")
+		Vector2i(0, -1):
+			play("walk_up")
+		Vector2i(1, 0):
+			play("walk_left")
+			scale.x = -1
+		Vector2i(-1, 0):
+			play("walk_left")
+
+	if lerp > 0:
+		lerp = clampf(lerp - dt / %AITimer.wait_time, 0.0, 1.0)
+	else:
+		stop()
+
 	%EnemyGoals.add_goal(self, 8)
 
 	%PlayerGoals.finish()
